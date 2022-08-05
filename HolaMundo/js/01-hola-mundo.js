d396fea2b3c96001b7924e18e86b2d90ba2faaa4
Diana <diana1310fg@gmail.com>
2022-08-05 11:11:09 -0500

    Variables y Comentarios.

--- HolaMundo/js/01-hola-mundo.js
+++ HolaMundo/js/01-hola-mundo.js
@@ -1,4 +1,6 @@
 alert("Hola Mundo con JS");
 alert("Bienvenido(a) al curso.");
 document.write("Hola mundo desde fichero externo");
-console.log("Muestra en consola de navegador.")
+
+/*console.log("Muestra en consola de navegador.")
+*/
---
+++ HolaMundo/js/02-variables.js
@@ -0,0 +1,8 @@
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2100;
+
+var pais_continente = pais + ' y ' + continente; //concatenación
+
+console.log(pais, continente, antiguedad);
+alert(pais_continente);
