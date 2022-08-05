7696eca511c0b94984265adcb40de096aa394d51
Diana <diana1310fg@gmail.com>
2022-08-05 11:39:00 -0500

    Constantes.

---
+++ HolaMundo/js/04-constantes.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var web = "isglobal.com.mx";
+const ip = "192.88.0.12";
+
+web = "isglobal.com";
+//ip = "190.34.12.4"; //ERROR: Assignment to constant variable.
+
+console.log(web, ip);
