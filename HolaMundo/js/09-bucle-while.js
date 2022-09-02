d11e57394474ad61a31074c42c2cfe216b702667
Diana <diana1310fg@gmail.com>
2022-09-01 23:54:47 -0500

    Bucle DO WHILE.

--- HolaMundo/js/09-bucle-while.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var year = 2022;
-
-while (year <= 2050) {
-	console.log("Estamos en el año " + year);
-
-	year++;
-}
---
+++ HolaMundo/js/09-while.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// BUCLE WHILE
+var year = 2022;
+
+while (year <= 2050) {
+	console.log("Estamos en el año " + year);
+
+	year++;
+}
+
+
+
+// BUCLE DO WHILE
+var years = 30;
+
+do {
+	alert("SOLO CUANDO SEA DIFERENTE A 20.");
+	years--;
+} while (years > 25)
