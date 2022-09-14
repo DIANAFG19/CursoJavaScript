cbc310cd73a801ea1b529a3fd2099305fc0611d5
Diana <diana1310fg@gmail.com>
2022-09-13 23:12:31 -0500

    Alertas y ventana de ingreso de datos.

---
+++ HolaMundo/js/10-ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// ALERTA
+alert("Esta es una alerta!!");
+alert("Este es mi texto");
+
+// CONFIRMACIÓN
+var resultado = confirm("¿Está seguro de continuar?");
+console.log(resultado);
+
+// INGRESO DE DATOS
+var resultado2 = prompt("¿Qué edad tienes?", 18);
+console.log(typeof resultado2);
+console.log(resultado2);
