489b150bc16865109b27b2c869cc86d5f29858ab
Diana <diana1310fg@gmail.com>
2022-09-13 23:36:17 -0500

    Ejercicio 1: Número mayor y menor PLUS.

--- HolaMundo/js/11-ejercicio1.js
+++ HolaMundo/js/11-ejercicio1.js
@@ -2,20 +2,28 @@
 
 /*
 Programa que pida dos números y que nos diga cuál es el mayor, el menor o si son iguales.
+PLUS: Si lo que se ingresa no son números o son menores o iguales a cero, vuelva a pedir el número.
 */
 
 var numero1 = parseInt(prompt('Introduce el primer número: ', 0));
 var numero2 = parseInt(prompt('Introduce el segundo número: ', 0));
 
-console.log(numero1);
-console.log(numero2);
+console.log(numero1, numero2);
+
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)) {
+	numero1 = parseInt(prompt('Introduce el primer número: ', 0));
+	numero2 = parseInt(prompt('Introduce el segundo número: ', 0));
+}
+
 
 if (numero1 == numero2) {
 	alert("Los números son iguales.");
 } else if (numero1 > numero2) {
 	alert("El número mayor es " + numero1);
 	alert("El número menor es " + numero2);
-} else {
+} else if (numero2 > numero1) {
 	alert("El número mayor es " + numero2);
 	alert("El número menor es " + numero1);
+} else {
+	alert("Introduce números válidos.");
 }
