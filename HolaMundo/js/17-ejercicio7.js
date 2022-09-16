f776c068c399da91a8735be709a73e2a48890d7f
UserCMP <diana1310fg@gmail.com>
2022-09-15 23:53:13 -0500

    Ejercicio 7: Tablas de multiplicar PLUS.

--- HolaMundo/js/17-ejercicio7.js
+++ HolaMundo/js/17-ejercicio7.js
@@ -4,8 +4,18 @@
 Generar las tablas de multiplicar del número introducido en pantalla.
 */
 
+// DE UN SOLO NÚMERO.
 var numero = parseInt(prompt("Ingresa un número: ", 1));
 
+document.write("<h1>Tabla del " + numero + "</h1>")
 for (var i = 1; i <= 10; i++){
-	document.write("<h1>" + numero + " x " + i + " = " + (numero * i) + "</h1>")
+	document.write(numero + " x " + i + " = " + (numero * i) + "<br>")
+}
+
+// TODAS LAS TABLAS.
+for (var j = 1; j <= 10; j++){
+	document.write("<h1>Tabla del " + j + "</h1>")
+	for (var i = 1; i <= 10; i++){
+		document.write(j + " x " + i + " = " + (j * i) + "<br>")
+	}
 }
