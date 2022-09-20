c1367bff0d95fd66d0b59d488471f2b109075f19
Diana <diana1310fg@gmail.com>
2022-09-19 22:46:26 -0500

    Parámetros.

--- HolaMundo/js/19-funciones.js
+++ HolaMundo/js/19-funciones.js
@@ -3,12 +3,28 @@
 // FUNCIONES
 
 // Definición de la función.
-function calculadora () {
+function calculadoraPrueba () {
+	// Conjunto de instrucciones a ejecutar.
 	console.log("Hola, soy la calculadora.");
 	//return "Hola, soy la calculadora.";
 }
 // Invocar a la función.
-calculadora();
-calculadora();
+calculadoraPrueba();
+calculadoraPrueba();
 //console.log(calculadora());
 
+function calculadora (numero1, numero2) {
+	console.log("Suma: " + (numero1 + numero2));
+	console.log("Resta: " + (numero1 - numero2));
+	console.log("Multiplicación: " + (numero1 * numero2));
+	console.log("División: " + (numero1 / numero2));
+	console.log("**************************************");
+
+	//return "Soy la calculadora.";
+}
+
+calculadora(12, 6);
+
+for (var i = 1; i <= 10; i++) {
+	calculadora(i, 7);
+}
