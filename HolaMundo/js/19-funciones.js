62a516b82dc6cb2082921f0df91924c88e063a67
Diana <diana1310fg@gmail.com>
2022-09-19 22:53:35 -0500

    Parámetros opcionales.

--- HolaMundo/js/19-funciones.js
+++ HolaMundo/js/19-funciones.js
@@ -9,22 +9,35 @@ function calculadoraPrueba () {
 	//return "Hola, soy la calculadora.";
 }
 // Invocar a la función.
-calculadoraPrueba();
-calculadoraPrueba();
+// calculadoraPrueba();
+
 //console.log(calculadora());
 
-function calculadora (numero1, numero2) {
-	console.log("Suma: " + (numero1 + numero2));
-	console.log("Resta: " + (numero1 - numero2));
-	console.log("Multiplicación: " + (numero1 * numero2));
-	console.log("División: " + (numero1 / numero2));
-	console.log("**************************************");
+function calculadora (numero1, numero2, mostrar = false) {
+	if (!mostrar) {
+		console.log("Suma: " + (numero1 + numero2));
+		console.log("Resta: " + (numero1 - numero2));
+		console.log("Multiplicación: " + (numero1 * numero2));
+		console.log("División: " + (numero1 / numero2));
+		console.log(mostrar);
+		console.log("**************************************");
+	} else {
+		document.write("Suma: " + (numero1 + numero2) + "<br/>");
+		document.write("Resta: " + (numero1 - numero2) + "<br/>");
+		document.write("Multiplicación: " + (numero1 * numero2) + "<br/>");
+		document.write("División: " + (numero1 / numero2) + "<br/>");
+		document.write(mostrar + "<br/>");
+		document.write("**************************************");
+	}
+	
 
 	//return "Soy la calculadora.";
 }
 
 calculadora(12, 6);
+calculadora(20, 5, true);
 
-for (var i = 1; i <= 10; i++) {
+/*for (var i = 1; i <= 10; i++) {
 	calculadora(i, 7);
-}
+}*/
+
