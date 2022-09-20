5778f63cc46e646a97901af95611ee8ecef7c76e
Diana <diana1310fg@gmail.com>
2022-09-19 22:31:50 -0500

    Crear Funciones.

---
+++ HolaMundo/js/19-funciones.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// FUNCIONES
+
+// Definición de la función.
+function calculadora () {
+	console.log("Hola, soy la calculadora.");
+	//return "Hola, soy la calculadora.";
+}
+// Invocar a la función.
+calculadora();
+calculadora();
+//console.log(calculadora());
+
