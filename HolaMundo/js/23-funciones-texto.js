9fc1d5b1dd3a75d3848f224b192018a5eff593bb
Diana <diana1310fg@gmail.com>
2023-01-11 22:25:32 -0600

    Métodos de Búsqueda.

--- HolaMundo/js/23-funciones-texto.js
+++ HolaMundo/js/23-funciones-texto.js
@@ -1,8 +1,8 @@
 'use strict'
 
-// TRANSFORMACIÓN DE TEXTOS.
+///////////// TRANSFORMACIÓN DE TEXTOS.
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript,";
+var texto1 = "Bienvenido al curso de JavaScript, curso de Diana Flores, ";
 var texto2 = "es muy buen curso";
 
 // Convertir a String.
@@ -30,3 +30,36 @@ textoTotal = texto1.concat(" hola " + texto2);
 console.log(textoTotal);
 
 
+///////////// MÉTODOS DE BÚSQUEDA
+var busqueda = texto1.indexOf("curso");
+console.log(busqueda);
+
+busqueda = texto1.lastIndexOf("curso");
+console.log(busqueda);
+
+busqueda = texto1.search("curso");
+console.log(busqueda);
+
+busqueda = texto1.search("juan");
+console.log(busqueda);
+
+busqueda = texto1.match("curso");
+console.log(busqueda);
+
+busqueda = texto1.match(/curso/gi);
+console.log(busqueda);
+
+busqueda = texto1.substr(14, 5);
+console.log(busqueda);
+
+busqueda = texto1.charAt(44);
+console.log(busqueda);
+
+busqueda = texto1.startsWith("Bien");
+console.log(busqueda);
+
+busqueda = texto1.endsWith("Flores, ");
+console.log(busqueda);
+
+busqueda = texto1.includes("JavaScript");
+console.log(busqueda);
