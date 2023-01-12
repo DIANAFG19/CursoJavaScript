925563779a44cb480c36250e13d9dd6dd4ec958d
Diana <diana1310fg@gmail.com>
2023-01-11 23:45:06 -0600

    Arrays básicos.

---
+++ HolaMundo/js/25-arrays.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var nombres = ["Diana Laura", "Luis Fernando", "José de Jesús", "Víctor Hugo", "Juan Carlos", 52, true];
+var lenguajes = new Array("PHP", "JavaScript", "Java", "C#", "Go");
+
+console.log(nombres);
+console.log(lenguajes);
+
+console.log(nombres[2]);
--- HolaMundo/js/pruebas.js
+++
@@ -1,4 +0,0 @@
-
-for (var secuencia = 1; secuencia <= 20; secuencia +=2){
-	console.log(secuencia);
-}
